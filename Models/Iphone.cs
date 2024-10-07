7319046e6b0784bc1ee96561b611079ccace7056
Heitor Porto - portoheitor <linuxheitor@hotmail.com>
2024-10-07 15:59:43 -0300

    Finalizando Desafio Abstraindo Celular DIO

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,15 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria) { }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando Aplicativo {nomeApp} no Aparelo Iphone\n      -> {NomeModeloCompleto()}");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nok1 = new Nokia("(35)9999-8888", "Nokia-TijoloMASTER", "587889988-98", 4);
+
+nok1.InstalarAplicativo("XP INVESTIMENTOS");
+
+Console.WriteLine("\n");
+
+Iphone ipho1 = new Iphone("(35)8888-9898", "Iphone-$FacadaMaster", "6548644987-98", 3);
+ipho1.InstalarAplicativo("WhatsApp");
